8cd861360412257fb7d8171d50040d4e6a23321b
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-02-28 12:11:23 +0530

    learn break keyword

--- operatorAndForLoop/ForLoop.java
+++ operatorAndForLoop/ForLoop.java
@@ -14,10 +14,30 @@ public class ForLoop {
         //     System.out.println(i + " " + j);
         // }
 
+        // for(int i = 1; i < 5; i++) {
+        //     for(int j = 1; j < 5; j++) {
+        //         System.out.println(i + " " + j);
+        //     }
+        // }
+
+        // Break keyword
+
+        for(int i = 1; i <= 5; i++) {
+            if(i == 3) {
+                break;
+            }
+            System.out.println(i);
+        }
+
+
         for(int i = 1; i < 5; i++) {
             for(int j = 1; j < 5; j++) {
-                System.out.println(i + " " + j);
+                if(i == j) {
+                    break;
+                }
+                System.out.print(j + " ");
             }
+            System.out.println();
         }
 
     }
