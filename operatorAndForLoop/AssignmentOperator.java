32505c620d88e49d31642fa94a2d525728a98f00
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-02-28 10:21:05 +0530

    learn assignment operatot

---
+++ operatorAndForLoop/AssignmentOperator.java
@@ -0,0 +1,14 @@
+package operatorAndForLoop;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a = 20;
+        // a = a + 1; =  a += 1;
+        // a = a - 1; = a -= 1;
+        // a = a * 2; = a *= 2;
+        // a  = a / 2; = a /= 2;
+        
+        
+
+    }
+}
