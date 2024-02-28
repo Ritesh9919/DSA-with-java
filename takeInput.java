85a69be9a25af336291d18ab8e26e1766808e1cc
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-02-28 09:42:20 +0530

    learn scanner in java

---
+++ takeInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * takeInput
+ */
+public class takeInput {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char str = sc.next().charAt(0);
+        System.out.println(str);
+    }
+}
