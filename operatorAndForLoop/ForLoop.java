6c9d0cff4b9bab2b466415fe86d4c07eff647db4
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-02-28 11:14:06 +0530

    learn variation of for loop

--- operatorAndForLoop/ForLoop.java
+++ operatorAndForLoop/ForLoop.java
@@ -2,8 +2,23 @@ package operatorAndForLoop;
 
 public class ForLoop {
     public static void main(String[] args) {
-        for(int i = 0; i <= 5; i++) {
-            System.out.println(i);
+        // for loop
+        // for(int i = 0; i <= 5; i++) {
+        //     System.out.println(i);
+        // }
+
+
+        // Variation of for loop
+
+        // for(int i = 1,j=10; i < 5 && j < 15; i++,j++) {
+        //     System.out.println(i + " " + j);
+        // }
+
+        for(int i = 1; i < 5; i++) {
+            for(int j = 1; j < 5; j++) {
+                System.out.println(i + " " + j);
+            }
         }
+
     }
 }
