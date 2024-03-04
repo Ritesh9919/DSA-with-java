f132908613912d380b35685dfdea9955f20b29d1
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-04 16:18:14 +0530

    solved triplet sum problem

---
+++ array/TripletSum.java
@@ -0,0 +1,25 @@
+package array;
+
+public class TripletSum {
+
+    public static int tripletSum(int[] arr, int x) {
+        int ans = 0;
+        int n = arr.length;
+
+        for(int i = 0; i < n-2; i++) {
+            for(int j = i+1; j < n-1; j++) {
+                for(int k = j+1; k < n; k++) {
+                    if(arr[i] + arr[j] + arr[k] == x) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7 };
+        int x = 12;
+        System.out.println(tripletSum(arr, x));
+    }
+}
