564eb1f22911f97a6a7dbe71e542ef8fc5da5b5c
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-05 09:40:49 +0530

    solved selection sort problem

---
+++ searchingAndSorting/SelectionSort.java
@@ -0,0 +1,25 @@
+package searchingAndSorting;
+import java.util.*;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            int minPos = i;
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,5,7,1,9,0};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
