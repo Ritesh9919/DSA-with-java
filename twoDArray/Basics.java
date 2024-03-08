71f897a847a1c517f5fed13da228191558b6174d
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-08 19:40:23 +0530

    basic operation on 2darray

--- twoDArray/Basics.java
+++ twoDArray/Basics.java
@@ -12,9 +12,15 @@ public class Basics {
     }
     
     public static void main(String[] args) {
-        int[][] arr = {{1,2,3,4},
-                      {5,6,7,8},
-                      {9,10,11,12}};
-        print(arr);
+        // int[][] arr = {{1,2,3,4},
+        //               {5,6,7,8},
+        //               {9,10,11,12}};
+        // print(arr);
+        int[][] arr2d = new int[3][4];
+        arr2d[2][1] = 10;
+        System.out.println(arr2d[2][1]);
+        System.out.println(arr2d[3][1]);
+        System.out.println(arr2d[1][0]);
+
     }
 }
