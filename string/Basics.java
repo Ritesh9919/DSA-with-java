26bd0da529c6cd531efdcf11794069f09a69f6d4
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-11 13:22:26 +0530

    learned basics of string

--- string/Basics.java
+++ string/Basics.java
@@ -1,5 +1,16 @@
 package string;
 
 public class Basics {
-    
+    public static void main(String[] args) {
+        String str = "Ritesh";
+        String str2 = "";
+        String str3 = " ";
+        System.out.println(str.length());
+        System.out.println(str.charAt(0));
+        System.out.println(str2.length());
+        System.out.println(str3.length());
+        System.out.println(str.charAt(-1));
+        System.out.println(str.charAt(6));
+
+    }
 }
