37e36c32d45184222676d7de9e96e6b354b59462
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-11 13:46:23 +0530

    learned string substring func

---
+++ string/StringSubstringFunc.java
@@ -0,0 +1,10 @@
+package string;
+
+public class StringSubstringFunc {
+    public static void main(String[] args) {
+        String str1 = "Coding";
+        System.out.println(str1.substring(0,3));
+        System.out.println(str1.substring(1, str1.length()));
+        
+    }
+}
