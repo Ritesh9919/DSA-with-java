990d47cff9b4b032bad0803493a50c6f6add9a44
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-11 13:56:10 +0530

    printed all char of string

---
+++ string/PrintAllCharOfString.java
@@ -0,0 +1,15 @@
+package string;
+
+public class PrintAllCharOfString {
+
+    public static void print(String str) {
+        for(int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+    
+    }
+    public static void main(String[] args) {
+        String str = "Hello World";
+        print(str);
+    }
+}
