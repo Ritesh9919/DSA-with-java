19979bfb703b378cdab2f3b57ca89125dacb40fb
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-03-31 11:00:51 +0530

    learned final keyword

---
+++ oop1/finalKeyword/FinalKeyword.java
@@ -0,0 +1,12 @@
+package oop1.finalKeyword;
+
+public class FinalKeyword {
+    final int score = 10;
+    
+    public static void main(String[] args) {
+        FinalKeyword fk = new FinalKeyword();
+        // fk.score = 20; // you can't do 
+        
+
+    }
+}
