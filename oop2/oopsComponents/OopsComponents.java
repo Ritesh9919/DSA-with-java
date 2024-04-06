c49d5912f60d5cd28c7603b107bfbee26ceabe92
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-04-06 12:20:23 +0530

    learned all access modifier

--- oop2/oopsComponents/OopsComponents.java
+++ oop2/oopsComponents/OopsComponents.java
@@ -1,4 +1,4 @@
-
+//  COMPONENTS OF OOPS
 
 // 1.  Encapsulation
 // 2. Abstraction
@@ -8,3 +8,11 @@
 // 1. Hiding data from outside world
 // 2. Showing only necceesory thing to user
 // 3. Inherit properties from parent class
+
+// ACCESS MODIFIER
+
+// 1. public  // can access anywhere
+// 2. private // can access within same class
+// 2. deafualt // can access within package anywhere
+// 3. protected // can access within package anywhere and derived class too
+
