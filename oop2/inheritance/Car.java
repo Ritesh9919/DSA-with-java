cbecbded14a166cbda77f29c32d7859541485e51
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-04-06 12:29:06 +0530

    learned super keyword in oop

--- oop2/inheritance/Car.java
+++ oop2/inheritance/Car.java
@@ -2,8 +2,11 @@ package oop2.inheritance;
 
 public class Car extends Vehicle {
     int numDoors;
+    int maxSpeed;
 
     public void print() {
+        super.print();
+        
         System.out.println("Car " + "color " + getColor() + " maxSpeed " + maxSpeed + " numDoors " + numDoors);
     }
 }
