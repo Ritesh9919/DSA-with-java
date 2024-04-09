c035f5598035ca5f17c79636b95cc2f6dd19715e
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-04-09 12:51:48 +0530

    learned basic of gnerics

---
+++ oop3/generics/Pair.java
@@ -0,0 +1,33 @@
+package oop3.generics;
+
+public class Pair <T> {
+    private T first;
+    private T second;
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+
+    public static void main(String[] args) {
+        Pair p1 = new Pair(2, 5);
+        System.out.println(p1.getFirst() + " " + p1.getSecond());
+    
+    }
+}
