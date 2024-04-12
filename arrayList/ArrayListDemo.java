065df3251b8c2698062d98690222b476f768bed2
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-04-12 10:00:22 +0530

    learned some more operation on arraylist

--- arrayList/ArrayListDemo.java
+++ arrayList/ArrayListDemo.java
@@ -9,7 +9,17 @@ public class ArrayListDemo {
         arr.add(2);
         arr.add(3);
         arr.add(1, 50);
-        System.out.println(arr.size());
-        System.out.println(arr.get(2));
+        arr.remove(1);
+        arr.set(1, 10);
+        // System.out.println(arr.size());
+        // System.out.println(arr.get(2));
+
+        for(int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
