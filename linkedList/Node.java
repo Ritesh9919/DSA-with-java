c38125ada0a98bc82847d0950026547a0ee6cdbc
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-04-13 11:10:42 +0530

    created node class and builded linkedlist

---
+++ linkedList/Node.java
@@ -0,0 +1,32 @@
+package linkedList;
+
+public class Node<T> {
+     
+    T data;
+    Node next;
+
+    Node(T data) {
+        this.data = data;
+    }
+
+    public static void print(Node<Integer> head) {
+
+        Node temp = head;
+
+        while(temp != null) {
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Node<Integer> head = new Node<Integer>(10);
+        Node<Integer> n2 = new Node<Integer>(20);
+        Node<Integer> n3 = new Node<Integer>(30);
+        head.next = n2;
+        n2.next = n3;
+
+        print(head);
+
+    }
+}
