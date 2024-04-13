eb204bf0bf3a2e6ec015218825f082c58f9b3bb9
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-04-13 15:06:17 +0530

    solved print ith node data problem

--- linkedList/Node.java
+++ linkedList/Node.java
@@ -40,16 +40,35 @@ public class Node<T> {
 
         return result;
     }
+
+    public static void printIthNodeData(Node<Integer> head, int i) {
+        int count = 0;
+        Node<Integer> temp = head;
+
+        while(temp != null) {
+            if(count == i) {
+              System.out.println(temp.data);
+            }
+
+            count++;
+            temp = temp.next;
+        }
+    }
     
     public static void main(String[] args) {
         Node<Integer> head = new Node<Integer>(10);
         Node<Integer> n2 = new Node<Integer>(20);
         Node<Integer> n3 = new Node<Integer>(30);
+        Node<Integer> n4 = new Node<Integer>(40);
+        Node<Integer> n5 = new Node<Integer>(50);
         head.next = n2;
         n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
         // increment(head);
         // print(head);
         System.out.println(countNode(head));
+        printIthNodeData(head, 3);
 
     }
 }
