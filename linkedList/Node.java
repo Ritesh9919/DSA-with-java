a1709f2430500963c99fc9902c263db21570b090
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-04-14 13:28:40 +0530

    solved insert node problem

--- linkedList/Node.java
+++ linkedList/Node.java
@@ -85,23 +85,51 @@ public class Node<T> {
 
            return head;
     }
+
+    @SuppressWarnings("unchecked")
+    public static Node<Integer> insertNode(Node<Integer> head, int pos, int data) {
+          Node<Integer> newNode = new Node<Integer>(data);
+
+          if(pos == 0) {
+            newNode.next = head;
+            return newNode;
+          }
+
+          Node<Integer> prev = head;
+          int count = 0;
+
+          while(count < pos-1 && prev != null) {
+            prev = prev.next;
+            count++;
+          }
+
+          if(prev != null) {
+            Node<Integer> next = prev.next;
+            prev.next = newNode;
+            prev.next.next = next;
+          }
+
+          return head;
+    }
     
     public static void main(String[] args) {
-        // Node<Integer> head = new Node<Integer>(10);
-        // Node<Integer> n2 = new Node<Integer>(20);
-        // Node<Integer> n3 = new Node<Integer>(30);
-        // Node<Integer> n4 = new Node<Integer>(40);
-        // Node<Integer> n5 = new Node<Integer>(50);
-        // head.next = n2;
-        // n2.next = n3;
-        // n3.next = n4;
-        // n4.next = n5;
+        Node<Integer> n1 = new Node<Integer>(10);
+        Node<Integer> n2 = new Node<Integer>(20);
+        Node<Integer> n3 = new Node<Integer>(30);
+        Node<Integer> n4 = new Node<Integer>(40);
+        Node<Integer> n5 = new Node<Integer>(50);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
         // // increment(head);
         // // print(head);
         // System.out.println(countNode(head));
         // printIthNodeData(head, 3);
-        Node<Integer> head = takeInput();
+        // Node<Integer> head = takeInput();
+        Node<Integer> head = insertNode(n1, 3, 100);
         print(head);
+        
 
     }
 }
